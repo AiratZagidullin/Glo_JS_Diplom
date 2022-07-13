22300bd495862f67299f6ace26d0610444bedf6a
Airat Zagidullin <zagbomar@gmail.com>
2022-07-13 09:00:24 +0500

    Сделал карусель

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import modal from './modules/modal'
 import menu from './modules/menu'
 import slider from './modules/slider'
+import services from './modules/services'
 
 
 modal()
 menu()
 slider()
+services()
