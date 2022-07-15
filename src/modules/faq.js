7841503f9424360939ffbe0f13ced4ce6d93636c
Airat Zagidullin <zagbomar@gmail.com>
2022-07-15 15:36:37 +0500

    Исправил аккордеон

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -2,16 +2,15 @@ const faq = () => {
     const faqElem = document.querySelectorAll('.accordeon > .element')
 
     faqElem.forEach(elem => {
-        const faqBtn = elem.querySelector('.title')
-        const faqContent = elem.querySelector('.element-content')
 
-        faqBtn.addEventListener('click', () => {
+        elem.addEventListener('click', () => {
             if (elem.classList.contains('active')) {
                 elem.classList.remove('active')
-                faqContent.style.display = 'none'
             } else {
+                faqElem.forEach(other => {
+                    other.classList.remove('active')
+                })
                 elem.classList.add('active')
-                faqContent.style.display = 'block'
             }
         })
     })
